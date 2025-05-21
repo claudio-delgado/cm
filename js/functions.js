1db9b5bfb26fe03af28d9655cf888edc92a88dab
cdelgado <=>
2025-05-21 19:18:07 -0300

    Functions.js: Mejoras en funciones para calcular el tiempo requerido de una expedición, así como la probabilidad de descubrir un monte.

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,11 @@
-let expeditionRequiredTime = (succesfullExpeditions, horsesAssigned) => {
+let expeditionRequiredTime = (succesfullExpeditions, horsesAssigned, maxXp = 0, avgXp = 0) => {
     //Returns the amount of ingame hours needed for an expedition.
-    //It dependes on the current already succesfull expeditions done and mounted expeditionaries assigned.
-    let inGameTotalHours = 30 /*+ 1 * 24 * 7*/ + Math.pow(2, succesfullExpeditions + 1) / (horsesAssigned + 1)
-
+    //It dependes on the current already succesfull expeditions done, mounted expeditionaries assigned and expeditionaries xp.
+    let lastExpeditionsFactor = (Math.pow(2, succesfullExpeditions + 1) + 2)
+    let horsesFactor = (2*horsesAssigned + 1)
+    let XPFactor = maxXp / 2 + avgXp / 5
+    let inGameTotalHours = 2 + (lastExpeditionsFactor / (horsesFactor + XPFactor))
+    /*inGameTotalHours += 53 * 24 * 7 */ 
     let inGameTotalDays = inGameTotalHours / 24
     let inGameTotalWeeks = inGameTotalDays / 7
     let inGameTotalYears = inGameTotalWeeks / 52
@@ -10,8 +13,6 @@ let expeditionRequiredTime = (succesfullExpeditions, horsesAssigned) => {
     let inGameClockWeeksD = (inGameTotalYears - inGameClockYears) * 52, inGameClockWeeks = Math.floor(inGameClockWeeksD)
     let inGameClockDaysD = (inGameClockWeeksD - inGameClockWeeks) * 7, inGameClockDays = Math.floor(inGameClockDaysD)
     let inGameClockHours = Math.floor((inGameClockDaysD - inGameClockDays) * 24)
-    //let inGameClockHoursD = (inGameClockDaysD - inGameClockDays) * 24, inGameClockHours = Math.floor(inGameClockHoursD)
-    //let inGameClockMinutes = Math.floor((inGameClockHoursD - inGameClockHours) * 60)
     let realTotalDays = (inGameTotalHours * 1.79) / 60 / 60 / 24
     let realTotalHours = (inGameTotalHours * 1.79) / 60 / 60
     let realTotalMinutes = (inGameTotalHours * 1.79) / 60
@@ -49,7 +50,15 @@ let expeditionRequiredTime = (succesfullExpeditions, horsesAssigned) => {
     }
 }
 
-let resourcesExpeditionProbability = (succesfullExpeditions, expeditionaries) => {
-    let probability = (expeditionaries / (expeditionaries + succesfullExpeditions + 1)).toFixed(3)
+let resourcesExpeditionProbability = (succesfullExpeditions, expeditionaries, maxXp = 0, avgXp = 0) => {
+    //Returns the probability of finding a new mount in an expedition.
+    //It dependes on the current already succesfull expeditions done, the amount of expeditionaries assigned, and their xp.
+    let lastExpeditionsFactor = (succesfullExpeditions + 1)
+    let XPFactor = (-1 / (maxXp + 1)) + 1
+    let avgXPFactor = (-1 / (avgXp + 1)) + 1
+    let probability1stTerm = expeditionaries / (expeditionaries + lastExpeditionsFactor)
+    let probability2ndTerm = ((7 / 10) - (7 * expeditionaries / (10 * expeditionaries + 10))) * XPFactor
+    let probability3rdTerm = ((3 / 10) - (3 * expeditionaries / (10 * expeditionaries + 10))) * avgXPFactor
+    let probability = (probability1stTerm + probability2ndTerm + probability3rdTerm).toFixed(3)
     return probability
 }

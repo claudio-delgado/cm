550297adcb1681067440c16c2438b9fa5cf3bcc0
cdelgado <=>
2025-05-23 16:59:32 -0300

    Separación de variables y constantes de inicialización a archivo independiente de home.js

---
+++ js/init.js
@@ -0,0 +1,14 @@
+//Long term global variables
+var language = "ES", citizensAmount, citizensFemaleAmount, citizensMaleAmount
+var daysPassed = 0, dayPassed = weekPassed = false, searchingZone = zoneSearched = lifeStarted = false
+var colonyWaterReservoir = "", resourcesExpeditionsDone = 0, ruinsExpeditionsDone = 0, huntingMountDiscovered = false
+var wagonsAmount, horsesAmount
+var colonyScore, colonyLifeQuality
+//Constants
+const zoneSearchHoursNeeded = 1
+//User configuration variables
+var showModalZoneSearched = true
+//Temporary global variables
+var citizenIndex, expeditionIndex = 1
+//General dinamic panels
+var newExpeditionPanel, assignRolePanel
